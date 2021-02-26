c06ffca46e66d1b87a1d6b1d9cbd407ac90bc465
Erik Anderson <efa@eecs.berkeley.edu>
2021-02-26 13:21:15 -0800

    cleaned up uart_led main file

--- src/uart_led/src/main.py
+++ src/uart_led/src/main.py
@@ -14,40 +14,6 @@ import time
 # Imports - local source
 from UARTDriver import *
 
-#class UARTDriver():
-#    
-#    def __init__(self, **kwargs):
-#        self.ser = serial.Serial(**kwargs)
-#        #self.ser.port = port
-#        #self.ser.baudrate = baudrate
-#        #self.ser.timeout = 1 # Read timeout
-#        #self.ser.write_timeout = 1
-#        #self.ser.bytesize = 8
-#        #self.ser.parity = serial.PARITY_NONE
-#        #self.ser.rtscts = True
-#        #self.ser.stopbits = serial.STOPBITS_ONE
-#
-#    def open(self):
-#        self.ser.open()
-#    
-#    def close(self):
-#        self.ser.reset_input_buffer()
-#        self.ser.reset_output_buffer()
-#        self.ser.close()
-#
-#    def write_byte(self, address: int, data: int):
-#        #address = address.to_bytes(1, "big")
-#        #data = data.to_bytes(1, "big")
-#        #print([address, data])
-#        #self.ser.write([address, data])
-#        #print(bytes([address, data]))
-#        num_bytes = self.ser.write(bytes([address, data]))
-#        #print(f"Num bytes written: {num_bytes}")
-#    
-#    def read_byte(self, address: int) -> int:
-#        self.ser.write((address + 128).to_bytes(1, "big"))
-#        return int.from_bytes(self.ser.read(), "big")
-
 if __name__ == '__main__':
     drv = UARTDriver(
         port="/dev/ttyUSB0",
@@ -61,56 +27,13 @@ if __name__ == '__main__':
   
     print(f'Read leds 0: {drv.read_register("LED", "leds0")}')
     drv.write_register("LED", "leds0", "1")
-    
     drv.write_register("LED", "leds1", "1")
     print(f'Read leds 1: {drv.read_register("LED", "leds1")}')
-    
     print(f'Read all leds: {drv.read_all_bytes("LED", "leds1")}')
     
-    #drv.write_leds("10101010")
+    for i in range(16):
+        drv.write_register("LED", "leds0", f"{i:04b}")
+        drv.write_register("LED", "leds1", f"{i:04b}")
+        time.sleep(1)
 
-    #for i in range(100):
-    #    print(f"LED write: {i}")
-    #    drv.write_byte(0, i)
-    #    print(f"LED read: {drv.read_byte(0)}")
-    #    time.sleep(1)
-
-    # Sending single bytes
-    #for i in range(50):
-    #    print(f"[{i}] Sending 0x55")
-    #    drv.ser.write(bytes([0x55]))
-    #    time.sleep(2)
-    #    print(f"[{i}] Sending 0xaa")
-    #    drv.ser.write(bytes([0xaa]))
-    #    time.sleep(2)
-    #    #time.sleep(2)
-    #    #drv.write_byte(0, 0x55)
-    #    #print(drv.read_byte(0))
-    
-    #drv.ser.write(bytes([0x0]))
-    #drv.ser.write(bytes([0x55]))
-    
-    #for i in range(50):
-    #    drv.ser.write(bytes([i]))
-    #    time.sleep(2)
-    
-    #print(drv.write_byte(0, 0x55))
-    #time.sleep(1)
-    #drv.write_byte(0, 0xaa)
-
-    #drv.ser.write(bytes([128]))
-    #print(int(drv.ser.read()))
-    #drv.open()
-    #drv.write_byte(0, 0)
-    #time.sleep(1)
-    #print(hex(99))
-    #drv.write_byte(0, 99)
-    
-    ## Write something
-    #drv.ser.write(bytes([0]))
-    #print("Address byte written")
-    #drv.ser.write(bytes([0x55]))
-    #print("Data byte written")
-    #drv.write_byte(0, 0x55)
-    
     drv.close()
